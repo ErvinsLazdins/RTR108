83f0fb543733a6183e875d387a21ebe79b64a7d7
ErvinsLazdins <54942931+ErvinsLazdins@users.noreply.github.com>
2020-02-13 14:06:33 +0200

    Add files via upload

---
+++ test2.py
@@ -0,0 +1,38 @@
+x = 10
+
+if x > 0 :
+        print('x is positive')
+
+if x < 200:
+        print(x)
+print(x*x)
+
+for i in range(5):
+    print(i)
+
+a = 10
+
+vaards='Māris'
+uzvaards='Tērauds'
+print("Vārds, Uzvārds: %s %s %d"%(vaards,uzvaards,a))
+
+if x%2 == 0 :
+        print('x is even')
+else :
+    print('x is odd')
+y = 3
+if x < y:
+    print('x is less than y')
+elif x > y:
+    print('x is greater than y')
+else:
+    print('x and y are equal')
+
+
+type(input())
+if choice == 'a':
+    print('Bad guess')
+elif choice == 'b':
+    print('Good guess')
+elif choice == 'c':
+    print('Close, but not correct')
